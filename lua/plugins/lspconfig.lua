30b8a36f03242b3232c9d48169230a5da932e6db
lucas-sallada <lucas.salladadasilva@dcsg.com>
2026-05-06 09:47:37 -0400

    feat(lsp): switch Kotlin LSP to JetBrains kotlin-lsp
    Enable LazyVim's Kotlin extra for treesitter/ktlint/dap wiring, then
    override the LSP layer to disable the older fwcd kotlin_language_server
    and use the official JetBrains kotlin-lsp via brew (cmd: kotlin-lsp --stdio).
    
    Spec: docs/superpowers/specs/2026-05-06-kotlin-lsp-design.md

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,14 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      servers = {
+        -- The LazyVim Kotlin extra wires up the older fwcd kotlin-language-server.
+        -- We disable it and use the official JetBrains kotlin-lsp instead.
+        kotlin_language_server = false,
+        kotlin_lsp = {
+          cmd = { "kotlin-lsp", "--stdio" },
+        },
+      },
     },
   },
 }
